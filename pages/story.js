5eb7edb234bdf60e23d104141d09da287898e54a
Jeffrey Zhang <jeffrey346@gmail.com>
2022-12-04 00:28:08 -0500

    Finish ads

--- pages/story.js
+++ pages/story.js
@@ -1,5 +1,4 @@
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Story.module.css";
@@ -7,8 +6,10 @@ import { CaretRightOutlined } from "@ant-design/icons";
 // import testPic from "../public/testPic.png";
 import { useStateContext } from "../context/state";
 import { setGlobalState } from "../context/state";
-import { Button } from "antd";
+import { Button, Modal } from "antd";
 import story from "../public/story.json" assert { type: "json" };
+
+const API_KEY = "01a71dcd61f94b4da903c4b52475e1f3";
 
 export default function Home() {
   const state = useStateContext();
@@ -16,6 +17,19 @@ export default function Home() {
   const [pageNum, setPageNum] = useState(1);
   const pages = Object.keys(story);
   const page = story[pageNum];
+
+  const [ipData, setIpData] = useState(null);
+  useEffect(() => {
+    // Fetch IP data
+    fetch("https://api.ipgeolocation.io/ipgeo?apiKey=" + API_KEY)
+      .then((res) => res.json())
+      .then((data) => {
+        setIpData(data);
+      });
+  }, []);
+
+  const { city, state_prov, country_flag, latitude, longitude, isp } =
+    ipData || {};
 
   return (
     <div className={styles.container}>
@@ -31,6 +45,11 @@ export default function Home() {
           </Button>
         ))}
       </div>
+
+      {/* POPUP ADS */}
+      {page.has_location_ad && <LocationAd city={city} name={name} />}
+      {page.has_isp_ad && <ISPAd city={city} name={name} isp={isp} />}
+
       <main className={styles.main}>
         <div className={styles.visualsBox}>
           {page.background && (
@@ -78,3 +97,85 @@ export default function Home() {
     </div>
   );
 }
+
+const LocationAd = ({ city, name }) => {
+  // Fetch IP data
+  const [isModalOpen, setIsModalOpen] = useState(true);
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <>
+      <Modal
+        title="Meet HOT local singles in your area!"
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        footer={false}
+      >
+        <p>🚨🚨 HEY {name}! It's YOUR lucky night ... 🚨🚨</p>
+        {/* <p>💦 Come see Jason Lin ... 3km away in {city}. 💦</p> */}
+        <div className={styles.singlesList}>
+          <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="blank">
+            <Button className={styles.singleButton}>
+              💦 Jason Lin ... 3km away in {city}. 💦
+            </Button>
+          </a>
+          <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="blank">
+            <Button className={styles.singleButton}>
+              💦 Jennifer Lawrrence ... 5km away in {city}. 💦
+            </Button>
+          </a>
+          <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="blank">
+            <Button className={styles.singleButton}>
+              💦 Angelina Jolieee ... 2.3km away in {city}. 💦
+            </Button>
+          </a>
+        </div>
+      </Modal>
+    </>
+  );
+};
+
+const ISPAd = ({ city, name, isp }) => {
+  // Fetch IP data
+  const [isModalOpen, setIsModalOpen] = useState(true);
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <>
+      <Modal
+        title={`IS ${isp.toUpperCase()} FEELING SLOW? 🐢🐢`}
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        footer={false}
+        width={600}
+      >
+        <p>
+          🚨🚨 {name} ... HAS {isp.toUpperCase()} BEEN SLOW LATELY? 🐌🦥 ...
+          🚨🚨
+        </p>
+        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="blank">
+          CLICK HERE TO SWITCH FROM {isp.toUpperCase()} TO GIGA FAST INTERNET IN{" "}
+          {city.toUpperCase()} TODAY!
+        </a>
+      </Modal>
+    </>
+  );
+};
